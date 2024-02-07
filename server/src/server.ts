8237523352f08265dd143cdd61ce9f6c1b104ace
Braily Guzman <braily@outlook.de>
2024-02-06 22:10:36 -0500

    Fix cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,12 +20,7 @@ declare global {
     }
 }
 
-app.use(
-    cors({
-        origin: clientURL,
-        credentials: true,
-    })
-);
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
